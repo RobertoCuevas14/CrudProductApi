aa60746af1b77793896de8966ed89d2475ab002f
RobertoCuevas14 <202010486@itla.edu.do>
2024-09-05 15:59:51 -0400

    actualizado

--- CrudP/Entity/Product.cs
+++ CrudP/Entity/Product.cs
@@ -5,7 +5,7 @@ namespace CrudP.Entities
     public class Product
     {
         public int Id { get; set; }
-        public string Product { get; set; }
+        public string Name { get; set; }
 
         public  string LastName { get; set; }
 
